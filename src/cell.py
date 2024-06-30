27d9341a4eae96682485c133d289d82f67802802
gafagarion <gafagarion@gmail.com>
2024-06-30 18:43:37 +0200

    first commit before test

---
+++ src/cell.py
@@ -0,0 +1,49 @@
+from graphics import Line, Point, Window
+
+
+class Cell:
+    def __init__(self, win):
+        self.has_left_wall = True
+        self.has_right_wall = True
+        self.has_top_wall = True
+        self.has_bottom_wall = True
+        self._x1 = None
+        self._x2 = None
+        self._y1 = None
+        self._y2 = None
+        self._win = win
+
+    def draw(self, x1, y1, x2, y2):
+        self._x1 = x1
+        self._x2 = x2
+        self._y1 = y1
+        self._y2 = y2
+        if self.has_left_wall:
+            line = Line(Point(x1, y1), Point(x1, y2))
+            self._win.draw_line(line)
+        if self.has_right_wall:
+            line = Line(Point(x2, y1), Point(x2, y2))
+            self._win.draw_line(line, "black")
+        if self.has_top_wall:
+            line = Line(Point(x1, y1), Point(x2, y1))
+            self._win.draw_line(line, "black")
+        if self.has_bottom_wall:
+            line = Line(Point(x1, y2), Point(x2, y2))
+            self._win.draw_line(line, "black")
+
+    def draw_move(self, to_cell, undo=False):
+        from_cell_center_x, from_cell_center_y = self.__get_center()
+        to_cell_center_x, to_cell_center_y = to_cell.__get_center()
+        line = Line(
+            Point(from_cell_center_x, from_cell_center_y),
+            Point(to_cell_center_x, to_cell_center_y),
+        )
+        color = None
+        if not undo:
+            color = "red"
+        else:
+            color = "gray"
+        self._win.draw_line(line,color)
+
+    def __get_center(self):
+        return (self._x1 + self._x2) / 2, (self._y1 + self._y2) / 2
